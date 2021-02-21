22b31426fc268667b217d0feae43795a14c97d95
Will B <willhbjr@gmail.com>
2021-02-21 18:21:42 +0000

    removed if statement

--- index.js
+++ index.js
@@ -26,10 +26,24 @@ amountOfMeals = Number(amountOfMeals);
 while (i <1 || i>6){
 amountOfMeals = input.question("Invalid amount of meals. Enter a value between 1-6: ")
 }
+console.log("\n");
 //console.log(amountOfMeals);
 
 //Skill boosts: 
 // a. Use string formatting to print something more interesting than “[‘chicken’, ‘rice’, ‘peas’, ‘juice’, ‘apple’]” for the meal outputs.
 
+for (i = 0; i < amountOfMeals; i++){
+  let pantry = "";
+  pantry = proteinOptions[i] + ", " + grainOptions[i] + ", " + vegetableOptions[i]  + ", " + beverageOptions[i] + ", " + dessertOptions[i];
+let pantryArray = pantry.split(",");
+
+console.log(pantryArray)
+
+console.log(`Your meal for the day is ${pantryArray.slice(0,4)} and ${pantryArray[4]} for dessert!`);
+
+  //if (pantryArray.includes("kale"));{
+  //  console.log("Don't worry, you can have double chocolate tomorrow!")
+//  }
+  }
 // b. Use an “array of arrays” to store the food options in a ‘pantry’.
 // c. Modify your code to check each meal for kale.  If present, after the meal output add, “Don’t worry, you can have double chocolate tomorrow.”
